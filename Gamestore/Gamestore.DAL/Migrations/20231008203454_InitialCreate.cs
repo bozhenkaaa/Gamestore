7b57ed961cd1b20361194035eb6a30fd97831289
Bozhena Diakiv <bozhena_diakiv@epam.com>
2023-10-09 00:19:22 +0300

    1

---
+++ Gamestore/Gamestore.DAL/Migrations/20231008203454_InitialCreate.cs
@@ -0,0 +1,160 @@
+﻿// <copyright file="20231008203454_InitialCreate.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+#nullable disable
+
+namespace Gamestore.DAL.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    /// <inheritdoc/>
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc/>
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ParentGenreId = table.Column<int>(type: "int", nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Genres_Genres_ParentGenreId",
+                        column: x => x.ParentGenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Platforms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Platforms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GameAlias = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GenreId = table.Column<int>(type: "int", nullable: false),
+                    PlatformId = table.Column<int>(type: "int", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Games_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Games_Platforms_PlatformId",
+                        column: x => x.PlatformId,
+                        principalTable: "Platforms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name", "ParentGenreId" },
+                values: new object[,]
+                {
+                    { 1, "Strategy", null },
+                    { 2, "RPG", null },
+                    { 3, "Sports", null },
+                    { 4, "Races", null },
+                    { 5, "Action", null },
+                    { 6, "Adventure", null },
+                    { 7, "Puzzle & Skill", null },
+                    { 8, "Misc", null },
+                });
+
+            migrationBuilder.InsertData(
+                table: "Platforms",
+                columns: new[] { "Id", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Mobile" },
+                    { 2, "Browser" },
+                    { 3, "Desktop" },
+                    { 4, "Console" },
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name", "ParentGenreId" },
+                values: new object[,]
+                {
+                    { 9, "RTS", 1 },
+                    { 10, "TBS", 1 },
+                    { 11, "Rally", 4 },
+                    { 12, "Arcade", 4 },
+                    { 13, "Formula", 4 },
+                    { 14, "Off-road", 4 },
+                    { 15, "FPS", 5 },
+                    { 16, "TPS", 5 },
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_GameAlias",
+                table: "Games",
+                column: "GameAlias",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_GenreId",
+                table: "Games",
+                column: "GenreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_PlatformId",
+                table: "Games",
+                column: "PlatformId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Genres_ParentGenreId",
+                table: "Genres",
+                column: "ParentGenreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Platforms_Type",
+                table: "Platforms",
+                column: "Type",
+                unique: true);
+        }
+
+        /// <inheritdoc/>
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+
+            migrationBuilder.DropTable(
+                name: "Platforms");
+        }
+    }
+}
